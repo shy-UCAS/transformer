9028b4ac17c43a74f53ae2954ef73c6de8f6c8ef
shy <sunhaoyang24@mails.ucas.ac.cn>
2026-01-06 16:55:05 +0800

    添加字符预测和梯度裁剪功能到RNN模型

--- RNN/text_preprocessing.py
+++ RNN/text_preprocessing.py
@@ -173,6 +173,30 @@ class RNNModelScratch:
     def begin_state(self, batch_size, device):
         return self.init_state(batch_size, self.num_hiddens, device)   
 
+def predict_ch8(prefix, num_preds, net, vocab, device):
+    """在prefix后面生成新字符"""
+    state = net.begin_state(batch_size=1, device=device)
+    outputs = [vocab[prefix[0]]]
+    get_inputs = lambda: torch.tensor([outputs[-1]], device=device).reshape((1, 1))
+    for y in prefix[1:]:
+        _, state = net(get_inputs(), state)
+        outputs.append(vocab[y])
+    for _ in range(num_preds):
+        y, state = net(get_inputs(), state)
+        outputs.append(int(y.argmax(dim=2).reshape(1)))
+    return ''.join([vocab.idx_to_token[i] for i in outputs])
+
+def grad_clipping(net, theta):
+    """裁剪梯度"""
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
 if __name__ == '__main__':
     batch_size, num_steps = 32, 35
     train_iter, vocab = load_data_time_machine(batch_size, num_steps)
